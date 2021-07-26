052bdccf59d3fe60be58fd89818a02671e1c361d
Marianne Westenthanner <marianne@westenthanner.de>
2021-07-26 11:43:51 +0200

    Frontend working with routing and basic main menu

---
+++ frontend/src/components/add-item.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import '../styles/add-item.css';
+
+function AddItem() {
+  return (
+    <div className="App">
+      Here you can add new items.
+    </div>
+  );
+}
+
+export default AddItem;
---
+++ frontend/src/components/items-list.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import '../styles/items-list.css';
+
+function ItemsList() {
+  return (
+    <div className="App">
+      Here you can find the items list.
+    </div>
+  );
+}
+
+export default ItemsList;
