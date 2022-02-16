414f166a279fdb21a7c1e06dc2df60b03bfbe747
Marianne Westenthanner <marianne@westenthanner.de>
2022-02-16 08:25:50 +0100

    Basic styling for search and add button, added recipe card component

---
+++ frontend/src/components/Items/RecipeCard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="recipe-card"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+
+})
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/components/Items/SearchFilter.vue
+++ frontend/src/components/Items/SearchFilter.vue
@@ -1,5 +1,8 @@
 <template>
-    <p>Here are search and filter functionality.</p>
+    <div class="search-box">
+        <svg enable-background="new 0 0 32 32" id="Editable-line" version="1.1" viewBox="0 0 32 32" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><circle cx="14" cy="14" fill="none" id="XMLID_42_" r="9" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/><line fill="none" id="XMLID_44_" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2" x1="27" x2="20.366" y1="27" y2="20.366"/></svg>
+        <input type="text" name="search" id="search" placeholder="Search pantry...">
+    </div>
 </template>
 
 <script lang="ts">
@@ -10,5 +13,31 @@ export default defineComponent({
 </script>
 
 <style scoped>
+.search-box {
+    display: flex;
+    align-items: center;
+}
+
+.search-box > svg {
+    height: 2rem;
+    width: auto;
+}
+
+#search {
+    border: none;
+    margin-left: 0.5rem;
+    padding: 1rem;
+    font-family: Inter;
+    font-size: 100%;
+    color: var(--dark-green);
+}
+
+#search:focus {
+    outline: none;
+}
+
+::placeholder {
+  color: var(--dark-green);
+}
 
 </style>
